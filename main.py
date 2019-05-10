637b03e874b74fc85f029de59143c542f8500b57
Rizwan Ahmad Lubis <rizwan.lubis@gmail.com>
2019-05-10 15:07:13 -0500

    Include Sentiment analysis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pos_tagging
+import json
 from paragraph_generator import get_random_paragraph
 from aidlib import do_ai_madlib
+from sentiment_analysis import analyze_tone
 
 BLANK_TOKEN = "_"
 
@@ -11,27 +13,39 @@ if __name__ == "__main__":
     removed, new_text = pos_tagging.generate_blanks(text)
     print("Here is the original text:")
     print(new_text)
+    # print("Here is the sentiment analysis")
+    # print(analyze_tone(text))
 
+    sentiment = json.loads(analyze_tone(text))
+    overall_tone = sentiment["document_tone"]["tones"][0]["tone_name"].lower()
+    sentence_sentiments = sentiment["sentences_tone"]
+    
     # #prompt the user for each black and store all input
-    # for i in removed:
-    #     pos = i[2]
-    #     prompt = "idk"
-    #     if pos in {"JJ", "JJR", "JJS"}:
-    #         prompt = "Type an adjective: "
-    #     elif pos in {"RB", "RBR", "RBS"}:
-    #         prompt = "Type an adverb: "
-    #     elif pos in {"VB", "VBD", "VBG", "VBN", "VBP", "VBZ"}:
-    #         prompt = "Type a verb: "
-    #     else:
-    #         prompt = "I messed up!"
+    for i in range(len(removed)):
+        pos = removed[i][2]
+        tones = sentence_sentiments[i]["tones"]
+        if len(tones) == 0:
+            tone = overall_tone
+        else:
+            tone = tones[0]["tone_name"].lower()
+        
+        prompt = "idk"
+        if pos in {"JJ", "JJR", "JJS"}:
+            prompt = "Type an adjective that exhibits the tone of " + tone + ": "
+        elif pos in {"RB", "RBR", "RBS"}:
+            prompt = "Type an adverb that exhibits the tone of " + tone + ": "
+        elif pos in {"VB", "VBD", "VBG", "VBN", "VBP", "VBZ"}:
+            prompt = "Type a verb: "
+        else:
+            prompt = "I messed up!"
 
-    #     response = input(prompt)
-    #     i.append(response)
+        response = input(prompt)
+        removed[i].append(response)
 
-    # print(removed)
+    print(removed)
 
-    #return completed paragraph
-    # print(pos_tagging.fillin_blanks(new_text, removed))
+    # return completed paragraph
+    print(pos_tagging.fillin_blanks(new_text, removed))
 
-    print(do_ai_madlib(new_text, BLANK_TOKEN))
+    # print(do_ai_madlib(new_text, BLANK_TOKEN))
     
